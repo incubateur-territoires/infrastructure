75a8ed9fcffda6fd675ef59bb40a434e42639e50
Gaël <gael.demette+git@qonfucius.team>
2021-07-13 18:13:40 +0200

    feat: Add S3 backend

--- source.sh
+++ source.sh
@@ -1,1 +1,3 @@
 export SCW_CONFIG_PATH=$HOME/.config/scw/config_incubateur-territoires.yaml
+export AWS_ACCESS_KEY_ID=$(cat $SCW_CONFIG_PATH | grep access_key | awk '{print $2}')
+export AWS_SECRET_ACCESS_KEY=$(cat $SCW_CONFIG_PATH | grep secret_key | awk '{print $2}')
